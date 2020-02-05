ea0cae4d100064f31f3696557779e48809223745
Luis <luiizmc@gmail.com>
2020-02-05 11:51:27 -0600

    Commit message

--- src/app/models/CategoriesHelper.ts
+++ src/app/models/CategoriesHelper.ts
@@ -2,14 +2,23 @@ import { Category } from "./Category";
 
 export class CategoriesHelper {
   static categories: Category[] = [
-    new Category("Available for Download", ""),
-    new Category("Diseño Grafico", ""),
-    new Category("Desarrollo de Software", ""),
-    new Category("Finanzas", ""),
-    new Category("Finanza personal", ""),
-    new Category("Marketing", ""),
-    new Category("Documentales", ""),
-    new Category("Drama", ""),
-    new Category("Audios")
+    new Category("EDUCACIÓN FINANCIERA", ""),
+    new Category("TECNOLOGÍA", ""),
+    new Category("FINANZAS", ""),
+    new Category("MKT Y PUBLICIDAD", ""),
+    new Category("MEDIO AMBIENTE", ""),
+    new Category("ARTE Y CULTURA", ""),
+    new Category("SALUD Y BIENESTAR", ""),
+    new Category("IDIOMAS"),
+    new Category("HUMANIDADES"),
+    new Category("HOGAR"),
+    new Category("TURISMO"),
+    new Category("DISEÑO"),
+    new Category("HISTORIA Y POLITICA"),
+    new Category("CIENCIA"),
+    new Category("VENTAS"),
+    new Category("EMPRENDIMIENTO Y ADMINISTRACION"),
+    new Category("LEGAL/JURIDICO"),
+    new Category("DEPORTES"),
   ];
 }
